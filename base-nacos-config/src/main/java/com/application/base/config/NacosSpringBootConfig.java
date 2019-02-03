8c97f03df2211f2562423440a3480dceae570253
rockygogo <sunping521@126.com>
2019-02-03 11:47:45 +0800

    base-nacos 实践

--- base-nacos-config/src/main/java/com/application/base/config/NacosSpringBootConfig.java
+++ base-nacos-config/src/main/java/com/application/base/config/NacosSpringBootConfig.java
@@ -1,8 +1,23 @@
 package com.application.base.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @Author: 孤狼
  * @desc:
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class NacosSpringBootConfig {
+	
+	/**
+	 * nacos 配置web配置检查入口.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSpringBootConfig.class, args);
+	}
+
 }
--- base-nacos-config/src/main/java/com/application/base/config/nacos/NacosConfigControllr.java
+++ base-nacos-config/src/main/java/com/application/base/config/nacos/NacosConfigControllr.java
@@ -2,6 +2,7 @@ package com.application.base.config.nacos;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @desc: 测试 nacos 配置的管理实现.
  */
 @Slf4j
-@RestController
+@RefreshScope
+@RestController("config")
 public class NacosConfigControllr {
 	
 	@Value("${default-key:}")
@@ -20,7 +22,7 @@ public class NacosConfigControllr {
 	
 	/**
 	 * 访问地址:
-	 *      http://127.0.0.1:8001/base-nacos-server/server/getSetProps
+	 *      http://127.0.0.1:8001/base-nacos-config/config/getSetProps
 	 * @return
 	 */
 	@GetMapping("/getSetProps")
